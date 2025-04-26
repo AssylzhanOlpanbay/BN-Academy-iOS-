d2ef8b62725590242348a4d060d031eadf1ffcb2
AssylzhanOlpanbay <asylzhan.olpanbai@mail.ru>
2025-04-26 19:43:46 +0500

    Initial Commit

---
+++ BN Academy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BN Academy
+//
+//  Created by Asyl Olpanbai on 26.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
